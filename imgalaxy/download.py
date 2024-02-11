453c5617a4ac30228836db93cc9e96cfc0b00b28
Jose Castagnino <jose@muttdata.ai>
2024-02-10 23:07:14 -0300

    add download.py

--- imgalaxy/download.py
+++ imgalaxy/download.py
@@ -6,7 +6,7 @@ from urllib.request import urlretrieve
 import click
 import requests
 
-from imgalaxy.cfg import BASE_URL, CHECKSUMS_FILENAME, DEFAULT_PATH, MANGA_SHA1SUM
+from imgalaxy.cfg import BASE_URL, CHECKSUMS_FILENAME, DATA_DIR, DEFAULT_PATH, MANGA_SHA1SUM
 
 logging.basicConfig(level='INFO')
 logger = logging.getLogger(f"imgalaxy.{__file__}")
@@ -68,11 +68,13 @@ def main(path, start):
     failed_checksums: dict = {}
     with click.progressbar(sha1sums, empty_char='☆', fill_char='★', width=87) as bar:
         for image in bar:
-            image_filename = str(image).split(' ')[2]
+            image_filename = str(image).split(' ')[2].strip()
+            response = requests.get(BASE_URL + image_filename, timeout=90)
+            image_filepath = DATA_DIR / image_filename
+            with open(image_filepath, 'wb') as f:
+                f.write(response.content)
+
             image_sha = str(image).split(' ')[0]
-            image_filepath = path / image_filename
-            urlretrieve(BASE_URL + image_filename, image_filepath)
-            breakpoint()
             if not verify_checksum(image_filepath, image_sha):
                 failed_checksums[image_filename] = image_sha
 
