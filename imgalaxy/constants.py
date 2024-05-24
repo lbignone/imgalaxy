c9a69fec546d9cd7c73def4bc62f0d673945e4f0
silico <ulises@stamm.bio>
2024-05-24 14:22:33 -0300

    fix sweep broken pipe error

--- imgalaxy/constants.py
+++ imgalaxy/constants.py
@@ -8,5 +8,5 @@ IMAGE_SIZE = 128
 MASK = 'bar_mask'
 TRAIN_WITH = 'only'
 MIN_VOTE = 3  # min votes that the most voted pixel of a mask must have to be a spiral arm (barred) galaxy
-THRESHOLD = 6  # min votes that a pixel must have to be clasified as a spiral arm (bar)
+THRESHOLD = 3  # min votes that a pixel must have to be clasified as a spiral arm (bar)
 PATIENCE = 10
